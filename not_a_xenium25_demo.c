54b41897b02dd78f78e0f6fe0f464540d4c5420d
yami-five <konrad.rykala.94@gmail.com>
2025-06-01 11:08:52 +0200

    almost there

--- not_a_xenium25_demo.c
+++ not_a_xenium25_demo.c
@@ -51,7 +51,7 @@ int main()
     cube->transformations = add_transformation(cube->transformations, &cube->transformationsNum, 0, 0, 0, 0);
 
     lightFactory = get_lightFactory();
-    PointLight *pointLight = lightFactory->create_point_light(0.0f, 50.0f, 50.0f, 1.0f, 0xffff);
+    PointLight *pointLight = lightFactory->create_point_light(0.0f, 50.0f, 50.0f, 2.0f, 0xffff);
 
     cameraFactory = get_cameraFactory();
     Camera *camera = cameraFactory->create_camera(0.0f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
